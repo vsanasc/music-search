800014435d1d85778e6f9437a4a2b7bd3e0c74b9
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2019-11-09 00:31:37 -0300

    Add URLSessionManagerRequestAPI

---
+++ MusicSearchKit/Repository/Network/Managers/URLSessionManagerRequestAPI.swift
@@ -0,0 +1,47 @@
+//
+//  URLSessionNetworkRequestAPI.swift
+//  SBrainKit
+//
+//  Created by Vitor S do Nascimento on 03-11-19.
+//  Copyright © 2019 Second Brain Project. All rights reserved.
+//
+
+import PromiseKit
+
+public class URLSessionManagerRequestAPI: ManagerRequestAPI {
+
+    private let manager = URLSession.shared
+
+    public init() { }
+
+    public func request(_ url: String, method: NetworkHTTPMethod, parameters: Parameters, headers: HTTPHeaders, completion: @escaping NetworkCompletion) {
+
+        guard let path = URL(string: url) else {
+            completion(nil, NetworkResult.error(NetworkError.urlError))
+            return
+        }
+
+        var request = URLRequest(url: path)
+        request.httpMethod = method.rawValue
+        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: [])
+
+        // Build Auth Header
+        for key in headers.keys {
+            request.addValue(key, forHTTPHeaderField: headers[key]!)
+        }
+
+        // Send Data Task
+        manager.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(nil, NetworkResult.error(error))
+                return
+            }
+            let httpResponse = response as? HTTPURLResponse
+            guard 200..<300 ~= httpResponse!.statusCode else {
+                completion(nil, NetworkResult.error(NetworkError.httpError))
+                return
+            }
+            completion(data, NetworkResult.success(httpResponse!))
+        }.resume()
+    }
+}
