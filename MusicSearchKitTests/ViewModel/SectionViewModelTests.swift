ce36801f1ed424562f9bbc472c6fe82435a722cf
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2019-11-09 06:33:15 -0300

    Add SongListViewModel tests

--- MusicSearchKitTests/ViewModel/SectionViewModelTests.swift
+++ MusicSearchKitTests/ViewModel/SectionViewModelTests.swift
@@ -6,13 +6,71 @@
 //
 
 import XCTest
+import RxSwift
 @testable import MusicSearchKit
 
 class SectionViewModelTests: XCTestCase {
 
-    func test_songListViewModel_initialize() {
-        let sut = SongListViewModel()
-        XCTAssertNotNil(sut)
+    private let disposeBag = DisposeBag()
+
+    override func setUp() {
+        super.setUp()
+
+        StubService.stubSongList()
+    }
+
+    func test_songListViewModel_loadSongs_success() {
+
+        let exp = expectation(description: "songListViewModel_loadSongs_success")
+        let repository = FakeSongListRemoteAPI()
+
+        let sut = SongListViewModel(repository: repository)
+        sut.loadSongs(search: "something")
+        sut
+            .dataList
+            .subscribe(
+                onNext: { (items) in
+                    XCTAssertEqual(items.count, 20)
+                    XCTAssertEqual(items.first?.trackName, "Track name")
+                    exp.fulfill()
+                }, onError: { (error) in
+                    XCTFail("\(error)")
+                    exp.fulfill()
+                }).disposed(by: disposeBag)
+
+        waitForExpectations(timeout: 3.0, handler: nil)
+    }
+
+    func test_songListViewModel_loadSongs_fail() {
+
+        let exp = expectation(description: "songListViewModel_loadSongs_fail")
+        let repository = FakeSongListRemoteAPIError()
+
+        let sut = SongListViewModel(repository: repository)
+        sut.loadSongs(search: "something")
+
+        sut
+            .dataList
+            .subscribe(
+                onNext: { (_) in
+                    XCTFail("That request must fail")
+                    exp.fulfill()
+                })
+                .disposed(by: disposeBag)
+        sut
+            .errorMessages
+            .subscribe(onNext: { (errorMessage) in
+                XCTAssertTrue(errorMessage.title != "")
+                XCTAssertTrue(errorMessage.message != "")
+                exp.fulfill()
+            })
+            .disposed(by: disposeBag)
+
+        waitForExpectations(timeout: 3.0, handler: nil)
+    }
+
+    override func tearDown() {
+        super.tearDown()
     }
 
 }
