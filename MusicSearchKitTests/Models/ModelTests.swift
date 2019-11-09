4dfd1988f454fd76ecf2120958a5b061933e6acb
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2019-11-08 22:48:00 -0300

    Add/move models tests

---
+++ MusicSearchKitTests/Models/ModelTests.swift
@@ -0,0 +1,29 @@
+//
+//  MusicSearchKitTests.swift
+//  MusicSearchKitTests
+//
+//  Created by Vitor S do Nascimento on 08-11-19.
+//
+
+import Foundation
+import XCTest
+@testable import MusicSearchKit
+
+class ModelTests: XCTestCase {
+
+    let decoder = JSONDecoderDateFormatter()
+    let bundle = Bundle(for: ModelTests.self)
+
+    func test_songModel_decoder() {
+        guard let dataJSON = bundle.readJSONData(for: "song") else {
+            XCTFail("Data json song doesn't be nil")
+            return
+        }
+        guard let sut = try? decoder.decode(SongModel.self, from: dataJSON) else {
+            XCTFail("Decoder fail")
+            return
+        }
+        XCTAssertNotNil(sut)
+        XCTAssertEqual(sut.artistName, "Californication")
+    }
+}
