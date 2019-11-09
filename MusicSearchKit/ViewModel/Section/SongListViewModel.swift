ab6203ea1731f39b94eea752423ccc38c6aeca95
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2019-11-09 06:32:42 -0300

    Add repository and loadSongs request

--- MusicSearchKit/ViewModel/Section/SongListViewModel.swift
+++ MusicSearchKit/ViewModel/Section/SongListViewModel.swift
@@ -6,7 +6,45 @@
 //
 
 import Foundation
+import RxSwift
+import PromiseKit
 
 public class SongListViewModel {
-    public init() { }
+
+    // MARK: - Dependecies
+    private let repository: SongListRemoteAPI
+
+    // MARK: - Messages
+    public var errorMessages: Observable<ErrorMessage> {
+        return errorMessagesSubject.asObservable()
+    }
+    private let errorMessagesSubject = PublishSubject<ErrorMessage>()
+
+    // MARK: - Data
+    public var dataList: Observable<[SongModelViewModel]> {
+        return dataListSubject.asObserver()
+    }
+    private let dataListSubject = PublishSubject<[SongModelViewModel]>()
+
+    // MARK: - Initialize
+    public init(repository: SongListRemoteAPI) {
+        self.repository = repository
+    }
+
+    public func loadSongs(search: String) {
+        repository
+            .getSongs(search: search)
+            .done { [weak self] items in
+                var elements = [SongModelViewModel]()
+                for item in items {
+                    elements.append(SongModelViewModel(songModel: item))
+                }
+                self?.dataListSubject.onNext(elements)
+            }
+            .catch { [weak self] _ in
+                let error = ErrorMessage()
+                self?.errorMessagesSubject.onNext(error)
+            }
+    }
+
 }
