4551a70ab341cb4c84894d54ee6393b09f1426e2
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2019-11-09 21:43:24 -0300

    Add optional value in all SongModel parameters

--- MusicSearchKit/Model/SongModel.swift
+++ MusicSearchKit/Model/SongModel.swift
@@ -15,32 +15,32 @@ public struct SongResponseModel: Decodable {
 }
 
 public struct SongModel: Decodable {
-    let wrapperType: String
-    let kind: String
-    let artistId: Int
-    let collectionId: Int
-    let trackId: Int
-    let artistName: String
-    let collectionName: String
-    let trackName: String
-    let artistViewUrl: URL
-    let collectionViewUrl: URL
-    let trackViewUrl: URL
-    let previewUrl: URL
-    let artworkUrl30: URL
-    let artworkUrl60: URL
-    let artworkUrl100: URL
-    let collectionPrice: Double
-    let trackPrice: Double
-    let releaseDate: Date
-    let discCount: Int
-    let discNumber: Int
-    let trackCount: Int
-    let trackNumber: Int
-    let trackTimeMillis: Int
-    let country: String
-    let currency: String
-    let primaryGenreName: String
+    let wrapperType: String?
+    let kind: String?
+    let artistId: Int?
+    let collectionId: Int?
+    let trackId: Int?
+    let artistName: String?
+    let collectionName: String?
+    let trackName: String?
+    let artistViewUrl: URL?
+    let collectionViewUrl: URL?
+    let trackViewUrl: URL?
+    let previewUrl: URL?
+    let artworkUrl30: URL?
+    let artworkUrl60: URL?
+    let artworkUrl100: URL?
+    let collectionPrice: Double?
+    let trackPrice: Double?
+    let releaseDate: Date?
+    let discCount: Int?
+    let discNumber: Int?
+    let trackCount: Int?
+    let trackNumber: Int?
+    let trackTimeMillis: Int?
+    let country: String?
+    let currency: String?
+    let primaryGenreName: String?
 
     enum CodingKeys: String, CodingKey {
         case wrapperType
