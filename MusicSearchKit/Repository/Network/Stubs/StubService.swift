3e8e985a753fdfcf979bb1ea75f89e81fa42d5d7
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2019-11-10 23:50:01 -0300

    Fix Stub search json and stub service

--- MusicSearchKit/Repository/Network/Stubs/StubService.swift
+++ MusicSearchKit/Repository/Network/Stubs/StubService.swift
@@ -10,10 +10,27 @@ import UIKit
 
 public class StubService {
 
+    private init() { }
+    /*
+    static func stubSongListFail() {
+
+        func builder404(request: URLRequest) -> Response {
+            let response = HTTPURLResponse(url: request.url!, statusCode: 404, httpVersion: nil, headerFields: nil)!
+            return .success(response, .noContent)
+
+        }
+        let endpoint = SongListEndpoint.search(term: "fail", limit: 20)
+        MockingjayProtocol.addStub(
+            matcher: http(endpoint.method.mockingjayMethod, uri: endpoint.fullpath),
+            delay: 1,
+            builder: builder404
+        )
+    }
+    */
     static func stubSongList() {
 
         let bundle = Bundle(for: StubService.self)
-        let endpoints: [SongListEndpoint] = [.search(term: "something", limit: 20)]
+        let endpoints: [SongListEndpoint] = [.search(term: "", limit: 20)]
         let image = UIImage(named: "artwork-demo.png", in: bundle, compatibleWith: nil)
         func builder(request: URLRequest) -> Response {
             let imageData: Data = (image?.pngData())!
